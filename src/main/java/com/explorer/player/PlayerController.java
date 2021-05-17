ec357b6414ccaa1e028783e51765c98951399265
Serge Bishyr <seriybg@gmail.com>
2021-05-17 13:51:54 +0300

    add hazelcast configuration

--- src/main/java/com/explorer/player/PlayerController.java
+++ src/main/java/com/explorer/player/PlayerController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
